26826ffaa33b901b056cf6373279203f58b5d984
Josué García <2995284560101@ingenieria.usac.edu.gt>
2024-03-16 12:07:27 -0600

    agregando jison con plantilla y scripts para ejecuciones, falta configurar la api

---
+++ Backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+var corsOptions = {
+    origin: '*'
+};
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors(corsOptions));
+
+//Routes 
+
+
+//Si no reconoce la ruta enviá el error
+app.use((req, res, next) => {
+    res.status(404).json({message: "Not found"});
+});
+
+module.exports = app;
---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+//const parser = require("./analizador/parser.js");
+
+
+// con alt + 96 se obtiene la comilla invertida para multilinea
+//let entrada =`
+//    2
+//`;
+//let resultado = parser.parse(entrada);
+//console.log(resultado);
+
+
+
+
+const app = require('./app.js');
+const PORT = 4000;
+app.listen(PORT);
+console.log(`Server on port ${PORT}`);
+
+
+//continuar con el video de la clase del 4/03
+//en el minuto 47:56
+//el tema continuaba con los routes y controller
